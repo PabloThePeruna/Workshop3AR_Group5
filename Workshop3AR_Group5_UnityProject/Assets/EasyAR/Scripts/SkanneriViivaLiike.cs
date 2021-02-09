bd32ebfda76eac05d358b27f4b369656f6309cca
Jaani Kekäläinen <44496339+jkekala@users.noreply.github.com>
2021-02-09 16:52:31 +0200

    Skanneri ja teksti

---
+++ Workshop3AR_Group5_UnityProject/Assets/EasyAR/Scripts/SkanneriViivaLiike.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkanneriViivaLiike : MonoBehaviour
+{
+    public float nopeus = 100;
+    public float maxSijainti = 65;
+    public float minSijainti = -65;
+
+    private RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        Vector2 position = rectTransform.anchoredPosition;
+
+        position.x += nopeus * Time.deltaTime;
+
+        if (position.x > maxSijainti)
+
+            position.x = minSijainti;
+
+        rectTransform.anchoredPosition = position;
+    }
+}
